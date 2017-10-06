bd4ce816c104ef4bc5df02d47b94f0146d0d77fd
racalhoun <racalhoun489@gmail.com>
2017-10-06 17:05:33 -0400

    typo fix

--- routes/homeController.js
+++ routes/homeController.js
@@ -1,12 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-
-
-
-
-router.get('/', (req, res,) =>{
-      res.render('home/home') 
-    //    users:users  
-    });
-    module.exports = router;
+router.get('/', (req, res) => {
+  res.render('home/home')
+  //    users:users  
+});
+module.exports = router;
